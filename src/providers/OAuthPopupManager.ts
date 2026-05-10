6337a9513ab14506a01454e7ddda4daff1b2202b
jagwar7 <com.official.akhter007@gmail.com>
2026-05-10 19:59:32 +0530

    FIX: 8

--- src/providers/OAuthPopupManager.ts
+++ src/providers/OAuthPopupManager.ts
@@ -14,12 +14,13 @@ export default class OAuthPopupManager implements IOAuthPopupManager{
     async handleAuthResponse(popup: Window, serverURL: string): Promise<string> {
     return new Promise((resolve, reject) => {
         let checkPopup: ReturnType<typeof setInterval>;
-
+        /*
         // CLEANUP INTERVAL
         const cleanup = () => {
             clearInterval(checkPopup);
             window.removeEventListener("message", popupEventResponse);
         };
+        */
 
 
         const popupEventResponse = (event: MessageEvent) => {
@@ -39,33 +40,33 @@ export default class OAuthPopupManager implements IOAuthPopupManager{
 
 
             if (data.type === "FLASHAUTH_TOKEN" && data.token) {
-                cleanup(); // Stop the timer immediately!
+              //  cleanup(); // Stop the timer immediately!
                 resolve(data.token);
             } else if (data.type === "FLASHAUTH_ERROR") {
-                cleanup(); // Stop the timer immediately!
+                //cleanup(); // Stop the timer immediately!
                 reject(new Error(data.error || "Authentication Failed"));
             }
         };
 
         window.addEventListener("message", popupEventResponse);
         const startTime = Date.now();
-/*        
-        checkPopup = setInterval(() => {
-            try{
-                if(Date.now()- startTime < 4000){
-                    console.log("closed under 4 Sec");
-                    return;
-                }
-                if (popup.closed) {
-                    cleanup();
-                    reject(new Error("FlashAuth: Popup closed by user #1"));
-                }
-            }catch(e){
-                console.warn("Popup status temporarily unreachable due to cross-origin redirect.");
-            }
-        }, 500)
+       
+        // checkPopup = setInterval(() => {
+        //     try{
+        //         if(Date.now()- startTime < 4000){
+        //             console.log("closed under 4 Sec");
+        //             return;
+        //         }
+        //         if (popup.closed) {
+        //             //cleanup();
+        //             reject(new Error("FlashAuth: Popup closed by user #1"));
+        //         }
+        //     }catch(e){
+        //         console.warn("Popup status temporarily unreachable due to cross-origin redirect.");
+        //     }
+        // }, 500)
     });
-*/
+
 }
 
 
